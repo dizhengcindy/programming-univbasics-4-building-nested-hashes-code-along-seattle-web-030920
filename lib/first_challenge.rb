930b525104a90924d3fd0e14017b554b4c345958
Di Zheng <dizhengcindy@gmail.com>
2020-02-13 03:13:06 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,10 +1,29 @@
 def first_challenge
    epic_tragedy = {
      :montage => {
-       
+       :patriarch =>{
+         :name =
+         
+       },
+       :matriarch => {
+         
+       },
+       :hero => {
+         
+       },
+       :hero_friends =>[]
      },
      :capulet => {
-       
+       :patriarch =>{
+         
+       },
+       :matriarch => {
+         
+       },
+       :hero => {
+         
+       },
+       :hero_friends =>[]
      }
      
    }
